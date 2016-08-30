8bec59ae8d9d738299202e8d5ae200743672aff8
Evan Moore <etmoore@gmail.com>
2016-08-29 20:47:24 -0600

    hello world

--- greeter.js
+++ greeter.js
@@ -1,5 +1,5 @@
 function greeter(person) {
     return 'Hello, ' + person + '!';
 }
-var user = [1, 2, 3];
+var user = 'Frank Shell';
 document.body.innerHTML = greeter(user);
--- greeter.ts
+++ greeter.ts
@@ -2,5 +2,5 @@ function greeter(person: string){
   return 'Hello, ' + person + '!';
 }
 
-var user = [1,2,3];
+var user = 'Frank Shell';
 document.body.innerHTML = greeter(user);
