8dd0dc23f9b7e7e7031668777dc6810c0bf8301d
Evan Moore <etmoore@gmail.com>
2016-08-29 21:04:26 -0600

    define Student class

--- greeter.js
+++ greeter.js
@@ -1,5 +1,16 @@
+// following the typescript quick-start tutorial:
+// https://www.typescriptlang.org/docs/tutorial.html
+var Student = (function () {
+    function Student(firstName, middleInitial, lastName) {
+        this.firstName = firstName;
+        this.middleInitial = middleInitial;
+        this.lastName = lastName;
+        this.fullName = firstName + ' ' + middleInitial + ' ' + lastName;
+    }
+    return Student;
+}());
 function greeter(person) {
     return 'Hello, ' + person.firstName + ' ' + person.lastName;
 }
-var user = { firstName: 'Chris', lastName: 'Samuelson' };
+var user = new Student("Jennifer", "Z.", "Jabble");
 document.body.innerHTML = greeter(user);
--- greeter.ts
+++ greeter.ts
@@ -1,6 +1,13 @@
 // following the typescript quick-start tutorial:
 // https://www.typescriptlang.org/docs/tutorial.html
 
+class Student {
+  fullName: string;
+  constructor(public firstName, public middleInitial, public lastName) {
+    this.fullName = firstName + ' ' + middleInitial + ' ' + lastName;
+  }
+}
+
 interface Person {
   firstName: string;
   lastName: string;
@@ -10,5 +17,5 @@ function greeter(person: Person){
   return 'Hello, ' + person.firstName + ' ' + person.lastName;
 }
 
-var user = { firstName: 'Chris', lastName: 'Samuelson' }
+var user = new Student("Jennifer", "Z.", "Jabble");
 document.body.innerHTML = greeter(user);
